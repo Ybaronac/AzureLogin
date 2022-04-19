92ec5ac213b93d69bd7194429c16829bc35b0555
Ybaronac <yemibace@gmail.com>
2022-04-19 16:46:14 -0500

    Primer commit para el logueo con Azure

---
+++ src/main/java/co/gov/icfes/autenticacionAD/AutenticacionAzureAD/controller/Ping.java
@@ -0,0 +1,22 @@
+package co.gov.icfes.autenticacionAD.AutenticacionAzureAD.controller;
+
+import co.gov.icfes.autenticacionAD.AutenticacionAzureAD.AutenticacionAzureAdApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Ping {
+    public static void main(String[] args) {
+        SpringApplication.run(AutenticacionAzureAdApplication.class, args);
+    }
+
+    @GetMapping("/pingTest")
+    public String hello(){
+        return "Ping OK";
+    }
+
+
+}
